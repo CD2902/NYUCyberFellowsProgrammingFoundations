2a303ffdea2106df847d9a9a5801f51e4e630c50
Chris D <cd2902@nyu.edu>
2020-12-19 10:38:05 -0500

    Completing Lesson 1

---
+++ Python/Lessons/Lesson1.1.py
@@ -0,0 +1,5 @@
+
+
+x = input("Please enter your name:")
+
+print("Hi", x, "welcome to Python")
---
+++ Python/Lessons/Lesson1.2.py
@@ -0,0 +1,11 @@
+print("Please enter two numbers in two separate lines:")
+
+firstNum_str = input()
+
+secondNum_str = input()
+additive = int(firstNum_str) + int(secondNum_str)
+
+print(firstNum_str, "+", secondNum_str,  "=",  str(additive))
+
+
+ 
