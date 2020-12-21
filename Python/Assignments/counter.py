a39b1a3c43be9c3f6841b9f39e4f4abbe3bb65a7
Chris D <cd2902@nyu.edu>
2020-12-21 01:21:34 -0500

    remove strings

--- Python/Assignments/counter.py
+++ Python/Assignments/counter.py
@@ -4,10 +4,10 @@
 
 # print("Please enter the number of coins")
 
-inp_quarters = input("# of quarters: ")
-inp_dimes =  input("# of dimes: ")
-inp_nickles = input("# of nickles: ")
-inp_pennies = input("# of pennies:  ")
+inp_quarters = input()
+inp_dimes =  input()
+inp_nickles = input()
+inp_pennies = input()
 
 totalDollars = float(inp_quarters)/4 + float(inp_dimes)/10 + float(inp_nickles)/20 + float(inp_pennies)/100
 totalcent = float("{:.2f}".format(totalDollars))  -  float("{:.0f}".format(totalDollars)) 
