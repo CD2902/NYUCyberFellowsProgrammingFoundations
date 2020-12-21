650be05b6e2a3453208c4d5fc60c5630e713d37f
Chris D <cd2902@nyu.edu>
2020-12-21 01:09:41 -0500

    remove period

--- Python/Assignments/counter.py
+++ Python/Assignments/counter.py
@@ -12,7 +12,7 @@ inp_pennies = input("# of pennies:  ")
 totalDollars = float(inp_quarters)/4 + float(inp_dimes)/10 + float(inp_nickles)/20 + float(inp_pennies)/100
 totalcent = float("{:.2f}".format(totalDollars))  -  float("{:.0f}".format(totalDollars)) 
 
-print("The total is", "{:.0f}".format(totalDollars), "dollars and", "{:.2f}".format(totalcent).replace('0.','') ,"cents.")
+print("The total is", "{:.0f}".format(totalDollars), "dollars and", "{:.2f}".format(totalcent).replace('0.','') ,"cents")
 
 
 
