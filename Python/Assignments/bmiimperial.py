88db8c8ab98b92fb33e8656cf9b533869abbe14b
Chris D <cd2902@nyu.edu>
2020-12-21 23:29:18 -0500

    wrap statement with a float

--- Python/Assignments/bmiimperial.py
+++ Python/Assignments/bmiimperial.py
@@ -1,7 +1,7 @@
-inp_weight = int(input("Please enter weight in kilograms: "))
+inp_weight = float(input("Please enter weight in kilograms: "))
 inp_height = float(input("Please enter height in meters: "))
 
 
 bmi_cal = (inp_weight*0.453592)/((inp_height*0.0254)**2)
 
-print("BMI is:","{:.10f}".format(bmi_cal)) 
+print("bmi is:","{:.10f}".format(bmi_cal)) 
