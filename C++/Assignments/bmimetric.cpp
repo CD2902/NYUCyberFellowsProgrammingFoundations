fc89eeadfe08b416d40ce2eed868e5ad6aaa0375
Chris D <cd2902@nyu.edu>
2020-12-22 11:13:58 -0500

    remove printStatements

--- C++/Assignments/bmimetric.cpp
+++ C++/Assignments/bmimetric.cpp
@@ -7,9 +7,9 @@ int main()
 
 double inp_weight, inp_height, bmiCal;
 
-cout << "Please enter weight in kilograms: ";
+//cout << "Please enter weight in kilograms: ";
 cin >> inp_weight;
-cout << "Please enter height om meters: ";
+//cout << "Please enter height om meters: ";
 cin >> inp_height;
 bmiCal = inp_weight/(inp_height*inp_height);
 cout << "BMI is: "<<setprecision(4)<<bmiCal<<endl;
