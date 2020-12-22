47eb1629f98b25023bb2f30dd596727939c8b050
Chris D <cd2902@nyu.edu>
2020-12-21 20:35:04 -0500

    update Calculation

--- Python/Assignments/coins.py
+++ Python/Assignments/coins.py
@@ -2,17 +2,24 @@ import math
 
 print("Please enter the amount of money to convert:")
 
-inp_num_dollars = input("# of dollars: ")
-inp_num_cents = input("# of cents: ")
+inp_num_dollars = float("{:.0f}".format(float(input("# of dollars: "))))
+inp_num_cents = float("{:.2f}".format(float(input("# of cents: "))))
+print ("inp_num_dollars =", inp_num_dollars, "\n", "inp_num_cents",inp_num_cents,"\n")
 
-total_money = float(inp_num_dollars) + (float(inp_num_cents)/100)
-dollars_to_quarters = total_money // 0.25
+total_money = inp_num_dollars + inp_num_cents/100
+#print("total_money", total_money)
+
+
+dollars_to_quarters = float(total_money) // 0.25
 c = total_money %.25
 leftover_dimes= c // 0.10
 c = c%.10
 leftover_nickles=c // 0.05
 c = c%.05
-leftover_pennies = c // 0.01
+#print("c", c)
+c = round(c,2)
+#print("c",c)
+leftover_pennies = c / 0.01
+#print("leftover_pennies =", leftover_pennies)
 
- 
-print("The coins are", int(math.ceil(dollars_to_quarters)) ,"quarters,", int(math.ceil(leftover_dimes)) ,"dimes,", int(math.ceil(leftover_nickles)),"nickels" ,"and" ,int(math.ceil(leftover_pennies)), "pennies")
+print("The coins are", int(dollars_to_quarters) ,"quarters,", int(leftover_dimes) ,"dimes,", int(leftover_nickles),"nickels" ,"and" ,int(leftover_pennies), "pennies")
