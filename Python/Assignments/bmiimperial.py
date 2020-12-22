908aa852b45d9500094952b9d96d25b32b0c7bff
Chris D <cd2902@nyu.edu>
2020-12-21 23:30:44 -0500

    remote imput prompts

--- Python/Assignments/bmiimperial.py
+++ Python/Assignments/bmiimperial.py
@@ -1,5 +1,5 @@
-inp_weight = float(input("Please enter weight in kilograms: "))
-inp_height = float(input("Please enter height in meters: "))
+inp_weight = float(input())
+inp_height = float(input())
 
 
 bmi_cal = (inp_weight*0.453592)/((inp_height*0.0254)**2)
