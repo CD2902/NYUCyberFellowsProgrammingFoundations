58f9c01f5ad797fe2e761da58466b5942506db9b
Chris D <cd2902@nyu.edu>
2020-12-30 13:25:48 -0500

    remove main

--- Python/Assignments/isleapyear.py
+++ Python/Assignments/isleapyear.py
@@ -1,14 +1,16 @@
-def isleapyear():
-  leapYear = int(input())
+def isleapyear(mybirthyear):
   if(leapYear % 400 == 0):
-    print("Year {0} was a leap year".format(leapYear))
+    #print("Year {0} was a leap year".format(mybirthyear))
+    return True
   elif(leapYear % 100  == 0):
-    print("Year {0} was not a leap year".format(leapYear))
+    #print("Year {0} was not a leap year".format(mybirthyear))
+    return False
   elif(leapYear % 4 == 0):
-    print("Year {0} was a leap year".format(leapYear))
+    #print("Year {0} was a leap year".format(mybirthyear))
+    return True
   else:
-    print("Year {0} was not a leap year".format(leapYear))
-
+    #print("Year {0} was not a leap year".format(mybirthyear))
+    return False
 
 
 
