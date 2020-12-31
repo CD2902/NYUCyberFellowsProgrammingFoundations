3e6166620b92e05fcb76f12f8e94f606b34edb80
Chris D <cd2902@nyu.edu>
2020-12-31 15:11:46 -0500

    Update String Splitter

--- C++/Assignments/stringsplitter.cpp
+++ C++/Assignments/stringsplitter.cpp
@@ -2,25 +2,34 @@
 # include <string.h>
 using namespace std; 
 
+string  remainingwords(string s)
+{
+
+int length = s.length();
+string firstword = s.substr(length);
+
+
+return firstword; 
+}
+
+
 int main()
 {
 
-string word;
-cin >> word;
 
+string s ;
+getline(cin, s);
 
-string firstword = word.substr(0, word.find(" "));
-cout <<firstword<<endl;
-
-
-
-
-
-
+int middle= s.length()/2;
+//cout << "Length  " << s.length() << endl;
+cout << "Middle character: "<<  s[middle]<<endl;
+cout << "First half: " << s.substr(0, middle)<<endl;
+cout <<  "Second half: " << s.substr(middle+1)<<endl;
 
 
 
-
-return 0; 
+return 0;
 }
+
+
 
