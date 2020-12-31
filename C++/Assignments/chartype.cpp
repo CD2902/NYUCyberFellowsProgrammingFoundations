19fdce50961ddf620ee35bf80bfc5ba2943060d5
Chris D <cd2902@nyu.edu>
2020-12-31 14:03:21 -0500

    Fix Typo

--- C++/Assignments/chartype.cpp
+++ C++/Assignments/chartype.cpp
@@ -11,11 +11,11 @@ cin >> userCh;
 if(userCh >= 'a' && userCh <= 'z')
 cout<<userCh << " is a lower case letter."<<endl;
 else if(userCh >= 'A' && userCh <= 'Z')
-cout<<userCh << " is an uppercase letter"<<endl;
+cout<<userCh << " is an upper case letter."<<endl;
 else if(userCh >= '0' && userCh <= '9')
-cout<<userCh << " is a digit"<<endl;
+cout<<userCh << " is a digit."<<endl;
 else
-cout<<userCh << " is a non-alphanumeric character"<<endl;
+cout<<userCh << " is a non-alphanumeric character."<<endl;
 
 return 0;
 }
