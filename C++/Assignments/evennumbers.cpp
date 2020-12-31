f143da911c97fe8a3ab842992df5cea8d059b36c
Chris D <cd2902@nyu.edu>
2020-12-31 00:29:02 -0500

    Upload evennumbers.cpp

---
+++ C++/Assignments/evennumbers.cpp
@@ -0,0 +1,21 @@
+# include  <iostream>
+using namespace std;
+
+
+int main()
+{
+
+  int x, inp_int;
+  cin >> inp_int;
+  x =0;
+  int count = 0;
+  while(x < inp_int)
+  {
+  count +=2;
+  cout <<count<<endl;
+  x++;
+  }
+  count = 0;
+
+return 0;
+}
