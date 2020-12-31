0634c7bed2ae449db2a30adad07ba889b50b3156
Chris D <cd2902@nyu.edu>
2020-12-31 00:08:06 -0500

    upload Max List

---
+++ C++/Assignments/maxinlst.cpp
@@ -0,0 +1,23 @@
+# include <iostream>
+# include <algorithm>
+using namespace std;
+
+int maxinlst(int lst, int size)
+{
+int max;
+for (int i =0; i <  size; i++)
+
+   
+  for(int j =0 i < size; j++)
+  {
+    if (lst[i] > lst[j])
+	
+     {
+       max = lst[i]
+     }
+
+  }
+  return max
+}
+
+
