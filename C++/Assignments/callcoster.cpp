592f94639fcd4178342c1554d6524aee5893bce0
Chris D <cd2902@nyu.edu>
2020-12-31 11:53:58 -0500

    Upload Call Cost Calculator

---
+++ C++/Assignments/callcoster.cpp
@@ -0,0 +1,67 @@
+# include <iostream>
+# include <iomanip>
+# include <string>
+using namespace std;
+
+int main()
+{
+int   inp_time, inp_duration;
+string inp_day;
+float rate =0, total; 
+
+cin >> inp_day;
+cin >>inp_time;
+cin >> inp_duration;
+
+
+
+
+if(inp_day == "Mon")
+{
+  if(800 < inp_time && inp_time < 1800)
+    rate = .40;
+  else if(inp_time < 800 || inp_time >1800)
+    rate = .25;
+}
+if(inp_day == "Tue")
+{
+  if(800 < inp_time && inp_time < 1800)
+    rate = .40;
+  else if(inp_time < 800 || inp_time >1800)
+    rate = .25;
+}
+if(inp_day == "Wed")
+{
+  if(800 < inp_time && inp_time < 1800)
+    rate = .40;
+  else if(inp_time < 800 || inp_time >1800)
+    rate = .25;
+}
+if(inp_day == "Thr")
+{
+  if(800 < inp_time && inp_time < 1800)
+    rate = .40;
+  else if(inp_time < 800 ||  inp_time >1800)
+      rate = .25;
+}
+if(inp_day == "Fri")
+{
+  if(800 < inp_time  && inp_time < 1800)
+    rate = .40;
+  else if(inp_time < 800 || inp_time >1800)
+
+    rate = .25;
+}
+if(inp_day == "Sat")
+    rate = .15;
+if(inp_day == "Sun")
+    rate = .15;
+
+total = rate * inp_duration;
+
+cout << "This call with cost $ "<< fixed << setprecision(2)<< total<< endl;
+
+
+return 0;
+}
+
