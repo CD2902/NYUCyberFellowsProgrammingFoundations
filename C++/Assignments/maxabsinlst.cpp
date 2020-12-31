62b0a84baf4639cc1318447c844b34aa9afd321e
Chris D <cd2902@nyu.edu>
2020-12-31 00:38:28 -0500

    Push Max Absolute In List Function

---
+++ C++/Assignments/maxabsinlst.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+# include <algorithm>
+using namespace std;
+
+int maxinlst(int* lst, int size)
+{
+int max;
+for (int i =0; i <  size; i++)
+  if(lst[i] < 0)
+  {
+    lst[i] = lst[i] * -1;
+  }
+   
+  for(int j =0; j < size; j++)
+  {
+    if (lst[i] > lst[j])
+        
+     {
+       max = lst[i];
+     }
+
+  }
+  return max;
+}
